9816a6443319427eca21bee6799adaf365588621
littleattok <164297061+littleattok@users.noreply.github.com>
2024-11-01 11:00:40 +0900

    스프링부트 환경 설정

---
+++ src/main/java/com/seoil/pk/poch/PochApplication.java
@@ -0,0 +1,13 @@
+package com.seoil.pk.poch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PochApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seoil/pk/poch/PochApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoil.pk.poch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
